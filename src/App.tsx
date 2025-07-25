017c17b6b924d2da61045b5e553391530f17102f
hypexeshomem <m.at.h.e.u.s.pei.xe.7.7@gmail.com>
2025-07-25 20:47:46 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,1 @@
-import React from 'react';
-
-function App() {
-  return (
-    <div>
-      <h1>AGORA FUNCIONA ESSA PORRA!</h1>
-    </div>
-  );
-}
-
-export default App;
+{"code":"rate-limited","message":"You have hit the rate limit. Please upgrade to keep chatting.","providerLimitHit":false,"isRetryable":true}
